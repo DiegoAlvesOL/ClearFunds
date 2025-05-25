138f3a2082200a7d13e3cc231cfc30d9c02d644d
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-05-25 23:52:33 +0100

    refactor (merchant): update merchant CLI to use clear prompts and rename tax_id to fee_rate

--- models/merchant.py
+++ models/merchant.py
@@ -1,16 +1,16 @@
 
 
 class Merchant:
-    def __init__(self,id_CF, company_name, company_number, vat_number, tax_number,bank_info):
+    def __init__(self,id_CF, company_name, company_number, vat_number, fee_rate,bank_info):
         self.id_CF = id_CF
         self.company_name = company_name
         self.company_number = company_number
         self.vat_number = vat_number
-        self.tax_number = tax_number
+        self.fee_rate = fee_rate
         self.bank_info = bank_info
 
     def __str__(self):
-        return "|"+str(self.id_CF) + "|" + self.company_name + "|" + str(self.company_number) + "|" + self.vat_number + "|" + str(self.tax_number) + "|" + str(self.bank_info) + "|"
+        return "|"+str(self.id_CF) + "|" + self.company_name + "|" + str(self.company_number) + "|" + self.vat_number + "|" + str(self.fee_rate) + "|" + str(self.bank_info) + "|"
 
 
     def to_dict(self):
@@ -19,6 +19,6 @@ class Merchant:
             "company_name": self.company_name,
             "company_number": self.company_number,
             "vat_number": self.vat_number,
-            "tax_number": self.tax_number,
+            "fee_rate": self.fee_rate,
             "bank_info": self.bank_info
         }
