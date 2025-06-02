a65c7d444c1ce9029044feb8ea44bd2af7aa4332
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-06-02 22:41:32 +0100

    refactor: update Transaction model to align with new processing logic
    - Improved object structure to support unlinked merchant fallback
    - Added support for transaction fee assignment based on type

--- models/transaction.py
+++ models/transaction.py
@@ -1,50 +1,54 @@
 class Transaction:
     def __init__(self,mti,
+                 transaction_type,
+                 merchant_id,
+                 company_number,
+                 mcc,
+                 fee_rate,
+                 terminal_id,
                  transaction_datetime,
                  stan,
-                 settlement_date,
-                 mcc,
-                 rrn,
+                 nsu,
                  auth_code,
-                 terminal_id,
-                 merchant_id,
                  currency_code,
-                 original_data,
-                 fees,
-                 cf_transaction_id,
+                 total_amount,
                  instalment_count,
-                 instalment_amount ):
+                 instalment_amount,
+                 original_data ):
         self.mti = mti
-        self.transaction_datatime = transaction_datetime
-        self.stan = stan
-        self.settlement_data = settlement_date
-        self.mcc = mcc
-        self.rrn = rrn
-        self.auth_code = auth_code
-        self.terminal_id = terminal_id
+        self.transaction_type = transaction_type
         self.merchant_id = merchant_id
+        self.company_number = company_number
+        self.mcc = mcc
+        self.fee_rate = fee_rate
+        self.terminal_id = terminal_id
+        self.transaction_datetime = transaction_datetime
+        self.stan = stan
+        self.nsu = nsu
+        self.auth_code = auth_code
         self.currency_code = currency_code
-        self.original_data = original_data
-        self.fees = fees
-        self.cf_transaction_id = cf_transaction_id
+        self.total_amount = total_amount
         self.instalment_count = instalment_count
         self.instalment_amount = instalment_amount
+        self.original_data = original_data
 
+    # Converte o objeto Transaction em um dicionário (Usado para savar os arquivos .JSON)
     def to_dict(self):
         return {
             "mti": self.mti,
-            "transaction_datatime": self.transaction_datatime,
-            "stan": self.stan,
-            "settlement_data": self.settlement_data,
-            "mcc": self.mcc,
-            "rrn": self.rrn,
-            "auth_code": self.auth_code,
-            "terminal_id": self.terminal_id,
+            "transaction_type": self.transaction_type,
             "merchant_id": self.merchant_id,
+            "company_number": self.company_number,
+            "mcc": self.mcc,
+            "fee_rate": self.fee_rate,
+            "terminal_id": self.terminal_id,
+            "transaction_datetime": self.transaction_datetime,
+            "stan": self.stan,
+            "nsu": self.nsu,
+            "auth_code": self.auth_code,
             "currency_code": self.currency_code,
-            "original_data": self.original_data,
-            "fees": self.fees,
-            "cf_transaction_id": self.cf_transaction_id,
+            "total_amount": self.total_amount,
             "instalment_count": self.instalment_count,
-            "instalment_amount": self.instalment_amount
+            "instalment_amount": self.instalment_amount,
+            "original_data": self.original_data
         }
