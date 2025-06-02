118057a6a980b7c39e8a8e0e2ee88b21c6e097f3
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-06-02 22:42:48 +0100

    refactor: enhance merchant service with validation and ID generation
    - Improved merchant existence check
    - Added ID generation logic and adjusted persistence structure

--- services/merchant_service.py
+++ services/merchant_service.py
@@ -1,13 +1,13 @@
-
 import json
 import os.path
 import random
 import string
 
+# Define os caminhos para os arquivos JSON utilizados
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_DIR,"../data/merchants.json")
 
-#
+# Carrega todos os merchants cadastrados
 def load_merchant():
     if os.path.exists(FILE_PATH):
         with open(FILE_PATH, "r") as file:
@@ -17,10 +17,12 @@ def load_merchant():
                 return []
     return []
 
+# Verifica se já existe um merchant com o company_number informado
 def merchant_exists(company_number):
     merchants =load_merchant()
     return any (merchant.get("company_number") == company_number for merchant in merchants)
 
+# Gera um ID único para o merchant com prefixo 'CF-'
 def generate_id():
     letter_digits_id = string.ascii_uppercase + string.digits
 
@@ -30,8 +32,7 @@ def generate_id():
         id_for_merchant = id_for_merchant + random.choice(letter_digits_id)
     return prefix + id_for_merchant
 
-
-
+# Salva um novo merchant no arquivo merchants.json
 def save_merchant(merchant_obj):
     merchant_data = merchant_obj.to_dict()
 
