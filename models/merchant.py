53abc2bb19245dea07d5539f16221815c1d3a044
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-06-02 22:40:08 +0100

    refactor: update Merchant model to include fee fields and improve structure
    - Added 'credit_fee' and 'debit_fee' to constructor and to_dict
    - Ensured consistency between object structure and data persistence

--- models/merchant.py
+++ models/merchant.py
@@ -6,7 +6,6 @@ class Merchant:
         self.company_name = company_name
         self.company_number = company_number
         self.vat_number = vat_number
-        # self.fee_rate = fee_rate
         self.debit_fee = debit_fee
         self.credit_fee = credit_fee
         self.bank_info = bank_info
@@ -14,14 +13,13 @@ class Merchant:
     def __str__(self):
         return "|"+str(self.id_CF) + "|" + self.company_name + "|" + str(self.company_number) + "|" + self.vat_number + "|" + str(self.debit_fee) + "|"+ str(self.credit_fee) + "|" + str(self.bank_info) + "|"
 
-
+    # Converte o objeto Merchant em um dicionário (Usado para savar os arquivos .JSON)
     def to_dict(self):
         return {
             "id_CF": self.id_CF,
             "company_name": self.company_name,
             "company_number": self.company_number,
             "vat_number": self.vat_number,
-            # "fee_rate": self.fee_rate,
             "debit_fee": self.debit_fee,
             "credit_fee": self.credit_fee,
             "bank_info": self.bank_info
